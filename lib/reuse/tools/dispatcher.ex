1ce25270bdb2bb3f5855b8fddb85d275a23c8b8d
Andrea Janes <ajanes@unibz.it>
2018-09-15 18:21:25 +0200

    Fixed documentation

--- lib/reuse/tools/dispatcher.ex
+++ lib/reuse/tools/dispatcher.ex
@@ -7,33 +7,33 @@
 # SPDX-License-Identifier: MIT
 # 
 
-@moduledoc """
-Implements a generic process pool, which executes processes as long as a
-producer function provides new tasks to execute. To use the this module, you
-have to call it using the following code:
-
-# Define a function that is called when everything is done.
-everything_done = fn -> IO.puts("Yeahh!") end
-
-# Define a function that returns a function if there is something to do, and
-:no_work when everything is done.
-
-producer = fn -> fn -> :timer.sleep(2000) end end
-
-# The work_id is passed to the producer, which is useful if the producer needs to know some particular parameter
-work_id = 1
-# Number of tasks that are executed in parallel
-number_of_workers = 10
-# Name of the dispatcher, must be unique if more dispatchers are used.
-dispatcher_name = :test
-
-Tools.Workers.work(dispatcher_name, number_of_workers, work_id, producer,
-everything_done)
-
-"""
-
 defmodule Tools.Dispatcher do
   use GenServer
+
+  @moduledoc """
+  Implements a generic process pool, which executes processes as long as a
+  producer function provides new tasks to execute. To use the this module, you
+  have to call it using the following code:
+
+  # Define a function that is called when everything is done.
+  everything_done = fn -> IO.puts("Yeahh!") end
+
+  # Define a function that returns a function if there is something to do, and
+  :no_work when everything is done.
+
+  producer = fn -> fn -> :timer.sleep(2000) end end
+
+  # The work_id is passed to the producer, which is useful if the producer needs to know some particular parameter
+  work_id = 1
+  # Number of tasks that are executed in parallel
+  number_of_workers = 10
+  # Name of the dispatcher, must be unique if more dispatchers are used.
+  dispatcher_name = :test
+
+  Tools.Workers.work(dispatcher_name, number_of_workers, work_id, producer,
+  everything_done)
+
+  """
 
   def start_link(name, producer) do
     GenServer.start_link(__MODULE__, producer, name: name)
